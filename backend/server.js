0c2925aabf3836758f8f767ff6ab75e17551745c
sujith varghese <sujithvargheseofficial250@gmail.com>
2023-08-06 11:40:01 +0530

    commited

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,15 @@ app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
 
+app.get("/api/products/slug/:slug", (req, res) => {
+  const product = data.products.find((x) => x.slug === req.params.slug);
+  if (product) {
+    res.send(product); // Return the found product instead of data.products
+  } else {
+    res.status(404).json({ message: "Product not found" });
+  }
+});
+
 const port = 5000;
 app.listen(port, () => {
   console.log(`server is connected on the port ${port}`);
